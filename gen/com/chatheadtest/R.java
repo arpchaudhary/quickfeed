2824359a503d51cf0c791994c6ea7933544ab3c4
arpchaudhary <arpit.pes@gmail.com>
2015-01-23 01:06:54 +0530

    Adding options support

--- gen/com/chatheadtest/R.java
+++ gen/com/chatheadtest/R.java
@@ -28,20 +28,25 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int dismiss_button=0x7f090007;
+        public static final int action_settings=0x7f09000d;
+        public static final int apply_button=0x7f090009;
         public static final int divider=0x7f090006;
+        public static final int feed_container=0x7f09000a;
         public static final int feed_holder=0x7f090004;
+        public static final int feed_text_view=0x7f09000c;
         public static final int options_container=0x7f090002;
         public static final int overlay_container=0x7f090001;
         public static final int processing_view=0x7f090005;
+        public static final int qf_logo=0x7f09000b;
         public static final int qf_title=0x7f090003;
         public static final int refresh_button=0x7f090008;
         public static final int startChatHead=0x7f090000;
+        public static final int stop_button=0x7f090007;
     }
     public static final class layout {
         public static final int activity_starter=0x7f030000;
         public static final int block_overlay=0x7f030001;
+        public static final int feed_overlay=0x7f030002;
     }
     public static final class menu {
         public static final int starter=0x7f080000;
@@ -49,11 +54,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int dismiss=0x7f060005;
+        public static final int apply=0x7f060007;
         public static final int hello_world=0x7f060002;
+        public static final int options_str=0x7f060008;
         public static final int qf_text=0x7f060003;
         public static final int refresh=0x7f060006;
         public static final int snooze=0x7f060004;
+        public static final int stop=0x7f060005;
     }
     public static final class style {
         /** 
--- src/com/chatheadtest/ChatHeadService.java
+++ src/com/chatheadtest/ChatHeadService.java
@@ -14,12 +14,14 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.AsyncTask;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -37,13 +39,19 @@ public class ChatHeadService extends Service implements OnClickListener,
 
 	private URL feedURL = null;
 	private Button refresh;
+	private Button stop;
+	private Button apply;
 	private TextView processingStatus;
-	
+
+	private String feedText = "This is some feed that should be shown # I dont know what to write here #";
+	private TextView feedTextView;
+	private TextView feedLogo;
+
 	private WindowManager windowManager;
 	private WindowManager.LayoutParams params;
 	private boolean isDragEvent;
 	private RelativeLayout blockOverlay;
-
+	private RelativeLayout feedOverlay;
 	private int initialX;
 	private int initialY;
 	private float initialTouchX;
@@ -55,62 +63,79 @@ public class ChatHeadService extends Service implements OnClickListener,
 	}
 
 	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		return START_STICKY;
+	}
+
+	@Override
 	public void onCreate() {
 		super.onCreate();
-		try{
-			feedURL = new URL("https://graph.facebook.com/me/statuses?access_token=CAACEdEose0cBANZAWUisjQNuG1plogPmZBWmvNvEKIIsDsgOG3rhcNLO1t2KbJxXZAWZBoV5aGttkk2Hw4NyvygZBh6QiTQXXgQTf6P8CiZCcCxYJHw1nZAyBkfYcT6sN1ZALUTSvFf7xubW7vI3ZAZBja9dM0wmeF0qGZAldOqUnGpjXJEXW5wj2prZC9GO91M5kOZBc4qzUcgy1vpSAwsOFGRFetZC59sc1xZBaYZD");
-		}catch(Exception e){
-			Toast.makeText(this, "Cannot parse URL",Toast.LENGTH_SHORT).show();
-		}
+		// try{
+		// feedURL = new
+		// URL("https://graph.facebook.com/me/statuses?access_token=CAACEdEose0cBANZAWUisjQNuG1plogPmZBWmvNvEKIIsDsgOG3rhcNLO1t2KbJxXZAWZBoV5aGttkk2Hw4NyvygZBh6QiTQXXgQTf6P8CiZCcCxYJHw1nZAyBkfYcT6sN1ZALUTSvFf7xubW7vI3ZAZBja9dM0wmeF0qGZAldOqUnGpjXJEXW5wj2prZC9GO91M5kOZBc4qzUcgy1vpSAwsOFGRFetZC59sc1xZBaYZD");
+		// }catch(Exception e){
+		// Toast.makeText(this, "Cannot parse URL",Toast.LENGTH_SHORT).show();
+		// }
 		windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
 		// load the block overlay here
 		LayoutInflater overlayInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		blockOverlay = (RelativeLayout) overlayInflater.inflate(
-				R.layout.block_overlay, null);
+		blockOverlay = null;
+		feedOverlay = (RelativeLayout) overlayInflater.inflate(
+				R.layout.feed_overlay, null);
 
-		refresh = (Button)blockOverlay.findViewById(R.id.refresh_button);
-		processingStatus = (TextView)blockOverlay.findViewById(R.id.processing_view);
+		feedTextView = (TextView) feedOverlay.findViewById(R.id.feed_text_view);
+		feedTextView.setText(feedText);
+		feedTextView.setSelected(true);
+		feedLogo = (TextView) feedOverlay.findViewById(R.id.qf_logo);
+		feedLogo.setOnClickListener(this);
+		// feedTextView.setOnClickListener(this);
+		// processingStatus =
+		// (TextView)blockOverlay.findViewById(R.id.processing_view);
 		setUpListeners();
 
 		params = new WindowManager.LayoutParams(
-				WindowManager.LayoutParams.WRAP_CONTENT,
+				WindowManager.LayoutParams.MATCH_PARENT,
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.TYPE_PHONE,
-				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-						| WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
 				PixelFormat.TRANSLUCENT);
 
-		params.gravity = Gravity.CENTER;
-		params.dimAmount = 0.1f;
-		//for initializing the alert to particular coordinates
-		// params.x = 0;
-		// params.y = 100;
+		// params.gravity = Gravity.TOP;
+		// params.dimAmount = 0.1f;
+		// for initializing the alert to particular coordinates
+		params.x = 0;
+		params.y = -755;
 
-		windowManager.addView(blockOverlay, params);
-		
-		
-		new FetchFeedTask().execute(feedURL);
+		windowManager.addView(feedOverlay, params);
+
+		// new FetchFeedTask().execute(feedURL);
 
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if (blockOverlay != null)
+		if(blockOverlay!=null)
 			windowManager.removeView(blockOverlay);
+		if (feedOverlay != null)
+			windowManager.removeView(feedOverlay);
+		
 	}
 
 	private void setUpListeners() {
-		blockOverlay.setOnTouchListener(this);
-		blockOverlay.setOnClickListener(this);
-		((Button) blockOverlay.findViewById(R.id.dismiss_button))
-				.setOnClickListener(this);
-		((TextView)blockOverlay.findViewById(R.id.qf_title)).setOnClickListener(this);
-		refresh.setOnClickListener(this);
-		//((ImageView) blockOverlay.findViewById(R.id.close_button)).setOnClickListener(this);
+		feedOverlay.setOnTouchListener(this);
+		feedOverlay.setOnClickListener(this);
+		// ((Button) blockOverlay.findViewById(R.id.dismiss_button))
+		// .setOnClickListener(this);
+		// ((TextView)blockOverlay.findViewById(R.id.qf_title)).setOnClickListener(this);
+		// refresh.setOnClickListener(this);
+		// ((ImageView)
+		// blockOverlay.findViewById(R.id.close_button)).setOnClickListener(this);
 
 	}
+	
+	
 
 	private void killService() {
 		stopSelf();
@@ -118,15 +143,55 @@ public class ChatHeadService extends Service implements OnClickListener,
 
 	@Override
 	public void onClick(View v) {
-		//The user was dragging the window; don't react
+		// The user was dragging the window; don't react
 		if (isDragEvent) {
 			isDragEvent = false;
 			return;
 		}
-		
+
 		switch (v.getId()) {
-		case R.id.dismiss_button:
+		case R.id.stop_button:
+			Toast.makeText(getApplicationContext(), "Terminating Feed",
+					Toast.LENGTH_SHORT).show();
 			killService();
+			break;
+
+		case R.id.qf_logo:
+			if (blockOverlay == null) {
+				LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+				blockOverlay = (RelativeLayout) inflater.inflate(
+						R.layout.block_overlay, null);
+				refresh = (Button) blockOverlay
+						.findViewById(R.id.refresh_button);
+				stop = (Button) blockOverlay
+						.findViewById(R.id.stop_button);
+				apply = (Button)blockOverlay.findViewById(R.id.apply_button);
+				
+				refresh.setOnClickListener(this);
+				stop.setOnClickListener(this);
+				apply.setOnClickListener(this);
+				WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+						WindowManager.LayoutParams.WRAP_CONTENT,
+						WindowManager.LayoutParams.WRAP_CONTENT,
+						WindowManager.LayoutParams.TYPE_PHONE,
+						WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+						PixelFormat.TRANSLUCENT);
+				params.dimAmount = 0.6f;
+				params.gravity = Gravity.CENTER;
+				windowManager.addView(blockOverlay, params);
+				feedLogo.setText("<-");
+
+			} else {
+				windowManager.removeView(blockOverlay);
+				blockOverlay = null;
+				refresh.setOnClickListener(null);
+				refresh = null;
+				stop.setOnClickListener(null);
+				stop = null;
+				apply.setOnClickListener(null);
+				apply=null;
+				feedLogo.setText("QF");
+			}
 			break;
 
 		case R.id.qf_title:
@@ -137,7 +202,16 @@ public class ChatHeadService extends Service implements OnClickListener,
 		case R.id.refresh_button:
 			Toast.makeText(getApplicationContext(), "Hit refresh button",
 					Toast.LENGTH_SHORT).show();
-			new FetchFeedTask().execute(feedURL);
+			
+			break;
+		case R.id.feed_text_view:
+			Toast.makeText(getApplicationContext(), "Don't Touch feed!!",
+					Toast.LENGTH_SHORT).show();
+			break;
+			
+		case R.id.apply_button:
+			Toast.makeText(getApplicationContext(), "Hit apply button",
+					Toast.LENGTH_SHORT).show();
 			break;
 		default:
 			// do nothing
@@ -145,13 +219,13 @@ public class ChatHeadService extends Service implements OnClickListener,
 		}
 
 	}
-	
-	private void enableRefresh(){
+
+	private void enableRefresh() {
 		refresh.setEnabled(true);
 		refresh.setTextColor(Color.WHITE);
 	}
-	
-	private void disableRefresh(){
+
+	private void disableRefresh() {
 		refresh.setEnabled(false);
 		refresh.setTextColor(Color.GRAY);
 	}
@@ -172,77 +246,79 @@ public class ChatHeadService extends Service implements OnClickListener,
 			isDragEvent = true;
 			params.x = initialX + (int) (event.getRawX() - initialTouchX);
 			params.y = initialY + (int) (event.getRawY() - initialTouchY);
-			windowManager.updateViewLayout(blockOverlay, params);
+			windowManager.updateViewLayout(feedOverlay, params);
 			return true;
 		}
 
 		return false;
 	};
-	
-	
+
 	private class FetchFeedTask extends AsyncTask<URL, Void, String> {
-	    protected void onPreExecute(){
-	    	processingStatus.setText("Loading...");
-	    	disableRefresh();
-	    }
-		
+		protected void onPreExecute() {
+			processingStatus.setText("Loading...");
+			disableRefresh();
+		}
+
 		protected String doInBackground(URL... urls) {
-	         int count = urls.length;
-	         HttpClient httpclient = new DefaultHttpClient();
-             HttpResponse response;
-             String responseString = null;
-	  
-	         for (int i = 0; i < count; i++) {
-	        	 
-	             try {
-	                 response = httpclient.execute(new HttpGet(urls[i].toURI()));
-	                 StatusLine statusLine = response.getStatusLine();
-	                 if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-	                     ByteArrayOutputStream out = new ByteArrayOutputStream();
-	                     response.getEntity().writeTo(out);
-	                     out.close();
-	                     responseString = out.toString();
-	                 } else{
-	                     //Closes the connection.
-	                     response.getEntity().getContent().close();
-	                     throw new IOException(statusLine.getReasonPhrase());
-	                 }
-	             } catch (ClientProtocolException e) {
-	                 responseString = "Client Protocol Exception";
-	             } catch (IOException e) {
-	                 //responseString = "IO exception";
-	                 responseString = e.toString();
-	             } catch(Exception e){
-	            	 //responseString = "General Exception";
-	            	 responseString = e.toString();
-	             }
-	             if (isCancelled()) break;
-	             
-	             // Escape early if cancel() is called
-	             
-	         }
-	         
-	         try{
-	        	 JSONObject jsonObj = new JSONObject(responseString);
-	         }catch(JSONException je){
-	        	 //TODO Handle this exception
-	         }
-	         
-	         return responseString;
-	     }
+			int count = urls.length;
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			String responseString = null;
 
-//	     protected void onProgressUpdate(Integer... progress) {
-//	         setProgressPercent(progress[0]);
-//	     }
+			for (int i = 0; i < count; i++) {
 
-	     protected void onPostExecute(String result) {
-	    	 enableRefresh();
-	         //Toast.makeText(getBaseContext(), "Hit completed", Toast.LENGTH_SHORT).show();
-	    	 if(result == null)
-	    		 processingStatus.setText("Error in retrieving text");
-	    	 else
-	    		 processingStatus.setText(result.substring(0, result.length()>300?300:result.length()));
-	     }
-	 }
+				try {
+					response = httpclient.execute(new HttpGet(urls[i].toURI()));
+					StatusLine statusLine = response.getStatusLine();
+					if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						response.getEntity().writeTo(out);
+						out.close();
+						responseString = out.toString();
+					} else {
+						// Closes the connection.
+						response.getEntity().getContent().close();
+						throw new IOException(statusLine.getReasonPhrase());
+					}
+				} catch (ClientProtocolException e) {
+					responseString = "Client Protocol Exception";
+				} catch (IOException e) {
+					// responseString = "IO exception";
+					responseString = e.toString();
+				} catch (Exception e) {
+					// responseString = "General Exception";
+					responseString = e.toString();
+				}
+				if (isCancelled())
+					break;
+
+				// Escape early if cancel() is called
+
+			}
+
+			try {
+				JSONObject jsonObj = new JSONObject(responseString);
+			} catch (JSONException je) {
+				// TODO Handle this exception
+			}
+
+			return responseString;
+		}
+
+		// protected void onProgressUpdate(Integer... progress) {
+		// setProgressPercent(progress[0]);
+		// }
+
+		protected void onPostExecute(String result) {
+			enableRefresh();
+			// Toast.makeText(getBaseContext(), "Hit completed",
+			// Toast.LENGTH_SHORT).show();
+			if (result == null)
+				processingStatus.setText("Error in retrieving text");
+			else
+				processingStatus.setText(result.substring(0,
+						result.length() > 300 ? 300 : result.length()));
+		}
+	}
 
 }
